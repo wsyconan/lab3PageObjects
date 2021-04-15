515ffcc87eaf17fe369caf49a1ea805880e77840
wsyconan <wsyconan@gmail.com>
2021-04-15 18:53:53 +0300

    Use TestNG to execute cucumber.

--- src/test/java/hw3/RunTestEx1.java
+++ src/test/java/hw3/RunTestEx1.java
@@ -1,11 +1,14 @@
 package hw3;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
+//import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
+//import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+//@RunWith(Cucumber.class)
+@Test
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"}, features = "src/test/resources/hw3/ex1.feature")
-public class RunTestEx1 {
+public class RunTestEx1 extends AbstractTestNGCucumberTests{
 
 }
--- src/test/java/hw3/RunTestEx2.java
+++ src/test/java/hw3/RunTestEx2.java
@@ -1,11 +1,15 @@
 package hw3;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
+//import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
+//import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
+@Test
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"}, features = "src/test/resources/hw3/ex2.feature")
-public class RunTestEx2 {
+public class RunTestEx2 extends AbstractTestNGCucumberTests {
 
 }
